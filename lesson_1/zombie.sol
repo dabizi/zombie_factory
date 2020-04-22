c11a66d677eb97c01a4918ffe34f2edb20632346
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-04-22 14:29:39 +0200

    Update zombie.sol

--- lesson_1/zombie.sol
+++ lesson_1/zombie.sol
@@ -1,7 +1,6 @@
-### All code comes from the excellent cryptozombies website ###
-### This is from the code from lesson 1 ###
-### See https://cryptozombies.io/en/course for more info ###
-
+// All code comes from the excellent cryptozombies website
+// This is from the code from lesson 1 
+// See https://cryptozombies.io/en/course for more info
 
 pragma solidity >=0.5.0 <0.6.0;
 
