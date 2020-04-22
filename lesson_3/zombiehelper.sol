ded4d868a1a3ba64893ab9012a1e10257b7daf6f
Janin Grandne <47568974+dabizi@users.noreply.github.com>
2020-04-22 19:14:39 +0200

    Create zombiehelper.sol

---
+++ lesson_3/zombiehelper.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+// All code comes from the excellent cryptozombies website
+// This is the code from lesson 3
+// See https://cryptozombies.io/en/course for more info
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    _;
+  }
+
+  function changeName(uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
+
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
+  }
+
+}
